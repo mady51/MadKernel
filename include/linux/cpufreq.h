e63fccdc23d2c9ec32f5cfc062f712e17a87f23c
Pranav Vashi <neobuddy89@gmail.com>
2017-08-08 11:12:02 +0200

    cpufreq: Add latest impulse governor
    Signed-off-by: andip71 <andreasp@gmx.de>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -553,7 +553,10 @@ extern struct cpufreq_governor cpufreq_gov_lionheart;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_lionheart)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLU_ACTIVE)
 extern struct cpufreq_governor cpufreq_gov_blu_active;
-#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_blu_active)
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_blu_active)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IMPULSE)
+extern struct cpufreq_governor cpufreq_gov_impulse;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_impulse)
 #endif
 
 /*********************************************************************
