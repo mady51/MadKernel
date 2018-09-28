bcbb4ea540f57e46d4cd84c5b8bd738a120a6314
root <root@instance-1.c.thematic-gift-200703.internal>
2018-09-28 12:21:37 +0530

    set highspeed freq for test

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -1795,8 +1795,8 @@ static int cpufreq_governor_interactive(struct cpufreq_policy *policy,
 		mutex_lock(&gov_lock);
 
 		freq_table = cpufreq_frequency_get_table(policy->cpu);
-//		if (!tunables->hispeed_freq)
-//			tunables->hispeed_freq = 1036800;
+		if (!tunables->hispeed_freq)
+			tunables->hispeed_freq = 1036800;
 
 		ppol = per_cpu(polinfo, policy->cpu);
 		ppol->policy = policy;
