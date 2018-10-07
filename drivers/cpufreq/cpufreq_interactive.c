396e39f0fc584497ebff8984a7107de72cedc515
root <root@instance-1.c.thematic-gift-200703.internal>
2018-10-07 12:51:39 +0530

    tweak highspeed freq

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -1796,7 +1796,7 @@ static int cpufreq_governor_interactive(struct cpufreq_policy *policy,
 
 		freq_table = cpufreq_frequency_get_table(policy->cpu);
 		if (!tunables->hispeed_freq)
-			tunables->hispeed_freq = policy->max;;
+			tunables->hispeed_freq = 1036800;
 
 		ppol = per_cpu(polinfo, policy->cpu);
 		ppol->policy = policy;
